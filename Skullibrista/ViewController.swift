c5b09bacc43073e302bf218e79eaeacb107373bb
Lucas <lucasbrito_9@hotmail.com>
2019-12-29 17:23:02 -0300

    Created project and added images.

---
+++ Skullibrista/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skullibrista
+//
+//  Created by Lucas Santana Brito on 29/12/19.
+//  Copyright © 2019 lsb.br. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
